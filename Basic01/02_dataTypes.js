e3ef66285ff51577ecf3a898b2935f57bb176c3a
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-07-02 02:08:57 +0530

    understood datatypes.

---
+++ Basic01/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+// ek baar "use strict" likhne se pura code new version mai hi aayega we can't change after that
+
+// alert (4+4) // we are using nodejs , not browser . Can't use it in nodejs.
+// alert is use for arithematic operations
+
+let name = "Ananya" // string datatype
+let age = 20 // number datatype
+let isLoggedIn = false // boolean datatype
+
+
+// Data Types
+ 
+// number = 1 to power 53
+// bigInt = for bigger numbers
+// string = ""
+// boolean = true/false
+// null = standalone value of empty
+// undefined = variable is defined but value is not
+// symbol = for uniqueness . Mostly used in recact.js
+
+
+// object =  a non-primitive datatype.
+
+
+//"typeof" data type shows the types
+console.log(typeof "Ananya"); //  type is a string
+console.log(typeof age); // type is a number
+
+console.log(typeof null); //type is object in js
+console.log(typeof undefined); //type is undefined in js
