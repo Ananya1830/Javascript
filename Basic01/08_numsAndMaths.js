b9f817b8df7966efd8f666d201dde74a10c31cef
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-07-12 23:58:21 +0530

    understanding nums and maths

--- Basic01/08_numsAndMaths.js
+++ Basic01/08_numsAndMaths.js
@@ -57,4 +57,4 @@ console.log(Math.floor((Math.random()*10)+1));
 const min = 10;
 const max = 20;
 
-console.log(Math.floor(Math.random() * (min - max +1)) + min);
+console.log(Math.floor(Math.random()*(min - max +1)) + min);
