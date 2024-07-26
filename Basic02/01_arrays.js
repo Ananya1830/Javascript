784d072621141db03188ded615d328f1b707512e
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-07-26 13:06:13 +0530

    Added some more info.

--- Basic02/01_arrays.js
+++ Basic02/01_arrays.js
@@ -53,24 +53,31 @@ console.log(typeof newArr); // But it will now changed to string
 
 
 //Slice method
-let array = [4,5,89,7,3,9]
-console.log("A " , array); //A  [ 4, 5, 89, 7, 3, 9 ]
+let array = [4, 5, 6, 7, 8, 9]
+console.log("A " , array); //A  [ 4, 5, 6, 7, 8, 9 ]
 
 let slicearr = array.slice(1,3);
-console.log(slicearr); //[ 5, 89 ]
-console.log("B ",array)// B  [ 4, 5, 89, 7, 3, 9 ]
+console.log(slicearr); //[ 5, 6 ]  It will print value  of 1st and 2nd position will not print 3rd.
+console.log("B ",array)// B  [ 4, 5, 6, 7, 8, 9 ]
 /* The slice method detaches the particular element as mentioned in the parameter as range and does not 
-include the last range. The slice() method does not manipulate the original array as well. */
+include the last range. The slice() method does not manipulate the original array as well. If we will print 
+the array again after using slice it will return the original value */
 
 
 // Splice
-let array2 = [14,75,7,45,9,8]
-console.log("A ", array2); // A  [ 14, 75, 7, 45, 9, 8 ]
-
+let array2 = [11, 13, 15, 17, 19]
+console.log("A ", array2); // A  [11, 13, 15, 17, 19]
 let splicearr = array2.splice(1,3)
-console.log(splicearr); // [ 75, 7, 45 ]
+console.log(splicearr); // [ 13, 15, 17 ]   It include all the range it return 1st , 2nd and 3rd
  
-console.log("B ",array2); // B  [ 14, 9, 8 ]
+console.log("B ",array2); // B  [17, 19 ]
 /* The splice method also detaches particular elements as mentioned in the parameter as range and it 
 does include the last range. The splice() method manipulates the original array as well by cutting off 
 the elements that are mentioned in the parameter. */
+
+/* In slice last range is not included.
+   But in splice last range is included.
+
+   In slice it doesnot manipulate the original array value returns the original value
+   But in splice it does manipulate the original array value and only returns the value which are not included 
+   in range. */
