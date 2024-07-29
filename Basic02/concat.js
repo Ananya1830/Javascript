9277dd3046067e1a167193c8a422018489fcfb9d
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-07-29 23:37:30 +0530

    Create concat.js

---
+++ Basic02/concat.js
@@ -0,0 +1,5 @@
+const marvelHeros = ["Captain America", "Ironman", "Spiderman"]
+const dcHeros = ["Batman", "Superman", "Flash"]
+
+const allHeros = marvelHeros.concat(dcHeros);
+console.log(allHeros);
