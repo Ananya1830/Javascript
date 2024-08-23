c228b472c2c3fda147e5f09d6462ec89f785e83f
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-08-23 23:59:05 +0530

    Update 04_objects.js

---
+++ Basic02/04_objects.js
@@ -0,0 +1,13 @@
+// singleton = In literals we can't create singleton but through constructor we can create it.
+// Object.create (This is the constructor method) will do this later
+
+
+// Object literals
+const myDetails = {
+    name: "Ananya" ,
+    age: 20 , 
+    location: "BBSR" ,
+    email: "ananya@.com" ,
+    isLoggedIn: false
+
+}
