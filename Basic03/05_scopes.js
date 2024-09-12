24b46ade4fcbfed623504f04c79e1b743fb23c23
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-09-12 23:53:54 +0530

    Update 05_scopes.js

--- Basic03/05_scopes.js
+++ Basic03/05_scopes.js
@@ -2,18 +2,25 @@
 
  // let us have an example with a nested function
 
- function numOne(){
-    const x = 45
-    function numTwo(){
-       let y = 10
-       console.log(x+y);
-    }
-    console.log(y); //  y is not defined
+ function one(){
+   const username = "Ananya"
 
-    numTwo()
+   function two(){
+       const website = "Github"
+       console.log(username);
+   }
+    //console.log(website);  This will not execute becoz it is defined above the scope here it should have to
+                            // defined above the {} scope 
+
+    two() // Ananya is output becoz in func. two we declared username
+
 }
 
-numOne() 
+ one() // It will not even execute becoz it is not declared in above function
 
-// From the above example we learned that in a nested function, the child function can have access to the variables of its parent function.
+/* From the above example we learned that in a nested function, the child function can have access to the 
+variables of its parent function. */
+
 // But the parent function can not have the accessibility for the variables of its child function.
+
+// Here one is parent and two is child
