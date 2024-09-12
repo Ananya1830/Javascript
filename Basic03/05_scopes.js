c18eee5827c9342da95cce3945fc1976186738b3
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-09-13 01:25:32 +0530

    Update 05_scopes.js

--- Basic03/05_scopes.js
+++ Basic03/05_scopes.js
@@ -24,3 +24,61 @@ variables of its parent function. */
 // But the parent function can not have the accessibility for the variables of its child function.
 
 // Here one is parent and two is child
+
+
+// Hum jitni baar function declare krte hai uske liye utni baar callstag or scope banta hai
+
+
+
+
+// We can also have an example for the nested if statements
+
+if (true) {
+   const firstName = "Ananya"
+   if (firstName === "Ananya") {
+       const lastName = " Mohapatra"
+        console.log(firstName + lastName);  // Output : Ananya Mohapatra (It wii execute becoz it inside scope)
+   }
+
+   // console.log(lastName); // It wii not execute becoz we declared it outside the scope where we mentioned it
+}
+
+// console.log(firstName); // It will not becoz it's completely outside of the function
+
+
+
+
+
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// Little discussions about hoisting
+
+function addOne(num){
+   return num+1
+ }
+
+ console.log(addOne(5)); // 6 
+
+ // What if we declare the function as it is and call or execute the function above the function?
+
+ console.log(addOne(5)); // 6
+ 
+ function addOne(num){  // Here we only declare the function
+    return num+1
+  }
+  // The ans is still 6.
+
+  // Let us store function into a variable and then execute
+  
+  console.log(addT(5)); // error becoz 
+
+  const addT = function(num){   // Here wedeclared it as we as we hold it in a variable.
+   return num+2
+  }
+  
+// This code will through an error as we are storing a function in a variable.
+// If we do so the variable becomes or is also called an expression.
+// Although it is an expression it is also a variable and we know variables are not recognized before its declaration.
+
