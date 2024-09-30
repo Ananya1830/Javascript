05a0c832e7149c7961995ae02b8119edfc6f627c
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-09-30 23:54:43 +0530

    Update 07_arrows.js

--- Basic03/07_arrows.js
+++ Basic03/07_arrows.js
@@ -20,6 +20,7 @@ console.log(add(8 , 4)); // 12
 // We can also write the regular or explicit arrow function by another way that is writing the return keyword and curly braces within the same line
 
 const add2 = (num1,num2) => {return num1+num2}
+
 console.log(add2(68 , 41)); // 109
 
 // If the function has only one statement and the statement return a value we can remove the brackets and the return keyword as well 
