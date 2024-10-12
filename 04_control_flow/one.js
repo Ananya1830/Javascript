824465d36750bcb70ca35bb741d4ee1c1fb032ad
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-10-12 23:45:04 +0530

    .

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+// if
+
+const isUserLoggedIn = true
+if (isUserLoggedIn) {
+
+}
+
+// < , > , <= , >= , ==(equal) , !=(not equal) , ===(find equals and type) , !=(negative)
+
+
+// if
+
+const isUserLoggedIn = true
+if (isUserLoggedIn) {
+
+}
+
+// < , > , <= , >= , ==(equal) , !=(not equal) , ===(find equals and type) , !=(negative)
