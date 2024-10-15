6fd7912c83737fbf878fe3b1a077a9c24e0dc374
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-10-15 23:40:54 +0530

    Create truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,7 @@
+// In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context.
+
+//  All values are truthy unless they are defined as falsy.
+
+// All the falsy values are
+
+// false , 0 , -0 , 0n (in Bigint) , "" ,null , undefined , NaN.
