1f7fbb16772eab2113b1958ef29306563dd4c86b
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-10-19 01:49:20 +0530

    Understanding while and do while.

---
+++ 05_iterations/two_while_doWhile.js
@@ -0,0 +1,33 @@
+// while
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+// In Array
+
+let myArray = [`Virat`, `JK` , `V`]
+
+let arr = 0 
+while (arr < myArray.length) {
+    console.log(`I love ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+
+// do while loop
+// 1st work will done then condition will be checked
+
+let score = 95
+
+do {
+   console.log(`Score is ${score}`);
+    score++
+} while (score<=100);
+// Rare Use
