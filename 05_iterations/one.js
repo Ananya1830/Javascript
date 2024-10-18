ec77bc7fea27bd8fff0a2a6266d3f0f2a64bc701
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-10-19 00:26:10 +0530

    Update one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,25 @@
 // for loop
 //Loops can execute a block of code a number of times.
 
-for (let i = 1; i < 10; i++) {
+for (let i = 0; i <= 10 ; i++) {
+    const element = i;
+    console.log(element);
+    
+}
+
+
+for (let i = 0; i <8 ; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is a odd number");
+    }
+    console.log(element);
+    
+}
+
+
+
+for (let i = 1; i <= 5; i++) {
     const element = i;
     
     for (let j = 1; j <= 10; j++) {
@@ -12,18 +30,29 @@ for (let i = 1; i < 10; i++) {
 }
 
 
+let myArray = ["IronMan" , "Captain america" , "Spiderman"]
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    // IronMan , Captain america , Spiderman
+}
+
+
+
 // break keyword
 
 for (let i = 0; i < 8; i++) {
     if(i === 5){
-        console.log(`Got the number 5`);
-        break
+        console.log(`Got number 5`);
+        break // code will stop after 5 comes
     }
     const element = i
     console.log(element);
     
 }
-/*
+/* op
 0
 1
 2
@@ -37,7 +66,7 @@ Got the number 5
 for (let i = 0; i < 7; i++) {
     if(i === 5){
         console.log(`got the 5`);
-        continue
+        continue // After 5 code will also continue
     }
     const element =i;
     console.log(element);
