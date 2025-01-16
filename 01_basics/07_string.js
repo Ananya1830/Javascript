b5cb9f5108c8a721718ee66f6e56da255bbc6afc
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2025-01-17 03:10:03 +0530

    Debugging

--- 01_basics/07_string.js
+++ 01_basics/07_string.js
@@ -1,33 +1,33 @@
 const name = "ananya"
 const repoCount = 50
 
- console.log(name + repoCount + " Value"); // This syntax is outdated so not recommended
+// console.log(name + repoCount + " Value"); // This syntax is outdated so not recommended
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)  // Hello my name is ananya and my repo count is 50
 // This is more readable and reliable so recommended.
 // This is string interpolation.
 
 const gameName = new String('ananya-mo-com')
 
-console.log(gameName[4]);
+console.log(gameName[4]); // y
 
-console.log(gameName.__proto__);
+console.log(gameName.__proto__); // {}
 
 
-console.log(gameName.length); // Total characters here [13]
-console.log(gameName.toUpperCase()); // Convert to uppercase
-console.log(gameName.charAt(2));   // Position
-console.log(gameName.indexOf('m')); // Character in which position
+console.log(gameName.length); // 13 ,,,, Total characters here 
+console.log(gameName.toUpperCase()); // ANANYA-MO-COM ,,, Convert to uppercase
+console.log(gameName.charAt(2));   // a ,,,, Position
+console.log(gameName.indexOf('m')); // 7 ,,,, Character in which position
 
 
 const newString = gameName.substring(1,5) 
 /* When we divide it into substring character from 0 to 4 will show last no means 5 will not include
  which is [anan]
 */
-console.log(newString);
+console.log(newString); // nany
 
 
-const anotherString = gameName.slice(-4, 3)
+const anotherString = gameName.slice(-5, 3)
 /* In slice we can give -ve numbers means it will start from reverse
 */
 console.log(anotherString);
@@ -39,9 +39,10 @@ console.log(newStringOne.trim()); // After using trim it will remove the startin
 
 const url = "https://ananya.com/ananya%20mohapatra"
 
-console.log(url.replace('%20', '-')) // We replace %20 into -
+console.log(url.replace('%20', '-')) // https://ananya.com/ananya-mohapatra ,,,, We replace %20 into -
 
-console.log(url.includes('sundar')) /* We can ask some keywords are here or not by writing its name it will show
-                                    true or false*/
+console.log(url.includes('sundar'))  // false
+/* We can ask some keywords are here or not by writing its name it will show true or false*/
 
-console.log(gameName.split('-')); // Here we split gameName bases on - in other ways can do this also.
+console.log(gameName.split('-')); // [ 'ananya', 'mo', 'com' ]
+// Here we split gameName bases on - in other ways can do this also.
